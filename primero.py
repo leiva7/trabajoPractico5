838a83cc7488bc6694024b346558231b362b42c0
Leiva Paula <leivapaula7@gmail.com>
2019-05-16 19:03:15 -0300

    primer borrador de clase alumno

--- primero.py
+++ primero.py
@@ -1,3 +1,8 @@
 print ("Hola mundo")
 print ("Feliz cumple, eu")
-
+class Alumno (object):
+    def __init__(self,nombre,apellido,DNI,matricula):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.DNI = DNI
+        self.matricula = matricula
