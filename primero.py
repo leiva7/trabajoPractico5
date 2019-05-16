fccabf42c98be8784708eb481ceb9bfc92e449eb
Leiva Paula <leivapaula7@gmail.com>
2019-05-16 19:31:09 -0300

    fusion de ambas ramas

--- primero.py
+++ primero.py
@@ -6,3 +6,6 @@ class Alumno (object):
         self.apellido = apellido
         self.DNI = DNI
         self.matricula = matricula
+Mati = Alumno(Matias,Sendra,39888444,333)
+print ("El DNI del alumno " + Mati.nombre + " es " + Mati.DNI)
+
