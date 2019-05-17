1029b68b3b8fb7271b4653eee9dd0f55f3b6c1e6
Leiva Paula <leivapaula7@gmail.com>
2019-05-17 19:42:59 -0300

    Agregadas funciones 4, 6 y 6

--- puntostp5.py
+++ puntostp5.py
@@ -1,3 +1,20 @@
 #Creando el archivo para hacer las funciones
-def hola(nombre):
-    print ("Hola " + nombre)
+
+def esVocal(x):
+    vocales = ["a","e","i","o","u"]
+    if x in vocales:
+        return "El caracter ingresado es vocal"
+    else:
+        return "El caracter ingresado es consonante o numero"
+
+def sumaElementos(lista):
+    resultado = 0
+    for e in lista:
+        resultado += e
+    return resultado
+    
+def multiplicoelementos(lista):
+    resultado = 1
+    for e in lista:
+        resultado = resultado*e
+    return resultado
