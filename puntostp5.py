7de2cfa15d433bf4cb2e9ec4f3d042bc488d71ef
maria paula montesino <montesinopaula01@gmail.com>
2019-05-17 19:48:15 -0300

    agregamos las funciones 1,2,3

--- puntostp5.py
+++ puntostp5.py
@@ -1,3 +1,29 @@
 #Creando el archivo para hacer las funciones
-def hola(nombre):
-    print ("Hola " + nombre)
+def max2 (a,b):
+    if a>b:
+        return a
+    else a>b:
+        return b
+
+## maximo de tres numeros
+def max3 (a,b,c):
+    if a>b  and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    elif c>a and c>b:
+        return c
+    else a==b==c:
+        return "son iguales"
+## largo de una lista
+
+
+def largo ( lista):
+resultado=0
+    for elementos in lista:
+        resultado= resultado + 1
+return resultado
+
+        
+
+
