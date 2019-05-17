6804cf3a9b93deddffa47f9db4b04b0128b82f3b
Leiva Paula <leivapaula7@gmail.com>
2019-05-17 20:35:04 -0300

    menu actualizado con las funciones 1 a 6

--- menu.py
+++ menu.py
@@ -1,4 +1,6 @@
 import puntostp5
+import otropunto
+
 while(True):
     print("""
             Seleccione una opcion:
@@ -14,17 +16,35 @@ while(True):
             """)
     op= input()
     if op=="1":
-        puntostp5.hola("Paula")
+        print("Ingrese el primer numero")
+        n1=input()
+        print("Ingrese el segundo numero")
+        n2=input()
+        otropunto.max2(n1,n2)
     elif op=="2":
-        print ("opcion 2")
+        print("Ingrese el primer numero")
+        n1=input()
+        print("Ingrese el segundo numero")
+        n2=input()
+        print("Ingrese el tercer numero")
+        n3=input()
+        otropunto.max2(n1,n2,n3)
     elif op=="3":
-        print ("opcion 3")
+        print ("ingrese una lista")
+        lista=input()
+        otropunto.largo(lista)
     elif op=="4":
-        print ("opcion 4")
+        print ("ingrese una vocal")
+        x=input()
+        puntostp5.esVocal(x)
     elif op=="5":
-        print ("opcion 5")
+        print ("ingrese una lista de numeros")
+        lista=input()
+        puntostp5.sumaElementos(lista)
     elif op=="6":
-        print ("opcion 6")
+        print ("ingrese una lista de numeros")
+        lista=input()
+        puntostp5.multiplicoelementos(lista)
     elif op=="7":
         print ("opcion 7")
     elif op=="8":
